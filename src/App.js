4ed14b3b83a5459914e8d32ffa75c0ba265918df
Camilo Molina <camilomolina8a@gmail.com>
2021-12-21 03:45:37 -0500

    (feat,style): input de Filter con sus estilos y boton limpiar busqueda, se creo componente TrendingGifs

--- src/App.js
+++ src/App.js
@@ -7,17 +7,28 @@ import Filter from "./components/Filter";
 
 function App() {
     const [modo, actualizarModo] = useState(true);
-    // const [busqueda,actualizarBusqueda] = useState("");
+    const [busqueda, actualizarBusqueda] = useState("");
 
     const manejarModo = () => {
         actualizarModo(!modo);
     };
+    const manejarBusqueda = (e) => {
+        actualizarBusqueda(e.target.value)
+    }
+    const cancelarBusqueda = () => {
+        actualizarBusqueda("")
+    }
 
     return (
         <div className={modo ? "App-container-light" : "App-container-dark"}>
             <Header modo={modo} manejarModo={manejarModo} />
 
-            <Filter modo={modo}/>
+            <Filter
+                modo={modo}
+                busqueda={busqueda}
+                manejarBusqueda ={manejarBusqueda}
+                cancelarBusqueda= {cancelarBusqueda}
+            />
         </div>
     );
 }
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import "../components/styles/Filter.css";
-import imgPersonas from "../assets/ilustra_header.png"
-import lupa from "../assets/icon-search-mod-noc.png"
 
-function Filter({modo}) {
+import imgPersonas from "../assets/ilustra_header.png";
+import lupa from "../assets/icon-search-mod-noc.png";
+import cerrar_dark from "../assets/eliminar-dark.png";
+import cerrar_light from "../assets/eliminar-light.png";
+
+function Filter({ modo, busqueda, manejarBusqueda, cancelarBusqueda}) {
     return (
         <div className="Filter-container">
-            <h1 className={ modo ? "Filter-title-light" : "Filter-title-dark"}>
+            <h1 className={modo ? "Filter-title-light" : "Filter-title-dark"}>
                 ¡Inspírate y busca los mejores <span>GIFS</span>!
             </h1>
 
@@ -16,15 +19,39 @@ function Filter({modo}) {
             </div>
 
             <form className="Filter-form">
-                <div>
-                    <input type="text" name="" id="" className={modo ? "Filter-form-input light" : "Filter-form-input dark"} placeholder="Buscar Gif"/>
-                    <div></div>
+                <div className="Filter-form-input-container">
+                    <input
+                        type="text"
+                        name=""
+                        id=""
+                        className={
+                            modo
+                                ? "Filter-form-input light"
+                                : "Filter-form-input dark"
+                        }
+                        placeholder="Buscar Gif"
+                        onChange={manejarBusqueda}
+                        value={busqueda}
+                    />
+                    {busqueda !== "" ? (
+                        <img
+                            src={modo ? cerrar_light : cerrar_dark}
+                            onClick={cancelarBusqueda}
+                            alt="Vaciar input"
+                        />
+                    ) : (
+                        ""
+                    )}
                 </div>
-                <button className={modo ? "Filter-button light" : "Filter-button dark"}  onClick={""}>
+                <button
+                    className={
+                        modo ? "Filter-button light" : "Filter-button dark"
+                    }
+                    onClick={""}
+                >
                     <img src={lupa} alt="" />
                 </button>
-            </form> 
-
+            </form>
         </div>
     );
 }
