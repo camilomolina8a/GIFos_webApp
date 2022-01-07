f1ebf928f3aad21f8f6712b401b1c5a906aeaa7b
Camilo Molina <camilomolina8a@gmail.com>
2022-01-07 03:07:26 -0500

    (checkpoint 2 funciona)

--- src/components/ResultsBySearch.js
+++ src/components/ResultsBySearch.js
@@ -6,12 +6,9 @@ import "../components/styles/ResultsBySearch.css";
 import { by_search_key, parameters, getData } from "../services";
 
 import Loader from "./Loader";
+import NoResultsFound from "./NoResultsFound";
 
-function ResultsBySearch({
-    modo,
-    palabraEnviada,
-    buscarBtn,
-}) {
+function ResultsBySearch({ modo, palabraEnviada, buscarBtn }) {
     const [dataBusqueda, actualizarDataBusqueda] = useState([]);
 
     const urlAPI = by_search_key + palabraEnviada + parameters;
@@ -33,58 +30,61 @@ function ResultsBySearch({
         }
     }, [urlAPI, buscarBtn]);
 
-
     return (
         <>
-            {palabraEnviada !== "" && buscarBtn && [
-                    dataBusqueda.length > 0 ? (
-                        <div className="ResultsBySearch-container">
-                            <h1
-                                className={
-                                    modo
-                                        ? "ResultsBySearch-text light"
-                                        : "ResultsBySearch-text dark"
-                                }
-                            >
-                                Resultados de tu Búsqueda
-                            </h1>
-                            <div
-                                className={
-                                    modo
-                                        ? "ResultsBySearch-results-container light"
-                                        : "ResultsBySearch-results-container dark"
-                                }
-                            >
-                                {dataBusqueda.map((data) => {
-                                    return (
-                                        <>
-                                            <a
-                                                href={data.bitly_gif_url}
-                                                target="_blank"
-                                                rel="noreferrer"
-                                                key={data.id}
-                                            >
-                                                <img
-                                                    className={
-                                                        modo
-                                                            ? "card light"
-                                                            : "card dark"
-                                                    }
-                                                    src={
-                                                        data.images.downsized
-                                                            .url
-                                                    }
-                                                    alt={data.title}
-                                                />
-                                            </a>
-                                        </>
-                                    );
-                                })}
+            {palabraEnviada !== "" &&  [
+                    dataBusqueda.length > 0
+                        ? [
+                            <div className="ResultsBySearch-container">
+                                <h1
+                                    className={
+                                        modo
+                                            ? "ResultsBySearch-text light"
+                                            : "ResultsBySearch-text dark"
+                                    }
+                                >
+                                    Resultados de tu Búsqueda
+                                </h1>
+                                <div
+                                    className={
+                                        modo
+                                            ? "ResultsBySearch-results-container light"
+                                            : "ResultsBySearch-results-container dark"
+                                    }
+                                >
+                                    {dataBusqueda.map((data) => {
+                                        return (
+                                            <div key={data.id}>
+                                                <a
+                                                    href={data.bitly_gif_url}
+                                                    target="_blank"
+                                                    rel="noreferrer"
+                                                >
+                                                    <img
+                                                        className={
+                                                            modo
+                                                                ? "card light"
+                                                                : "card dark"
+                                                        }
+                                                        src={
+                                                            data.images
+                                                                .downsized.url
+                                                        }
+                                                        alt={data.title}
+                                                    />
+                                                </a>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
                             </div>
-                        </div>
-                    ) : (
+                        ]
+
+                        : [
+
                         <Loader />
-                    ),
+                            
+                            ]
                 ]}
         </>
     );
